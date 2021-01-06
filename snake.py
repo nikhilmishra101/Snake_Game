a11196342c14ccdece92394ea2603499be4a7706
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-06 10:16:13 +0530

    Adding KeyStrokes

--- snake.py
+++ snake.py
@@ -21,3 +21,12 @@ class Snake:
                 new_y = self.segments[seg_num - 1].ycor()
                 self.segments[seg_num].goto(new_x, new_y)
             self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].setheading(90)
+    def down(self):
+        self.segments[0].setheading(-90)
+    def left(self):
+        self.segments[0].setheading(180)
+    def right(self):
+        self.segments[0].setheading(0)
