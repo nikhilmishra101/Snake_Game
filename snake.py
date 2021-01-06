25ab5ff9be58d2262fafbaf1c4011f3e95cc4461
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-06 10:19:00 +0530

    Adding Constants

--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -23,10 +24,10 @@ class Snake:
             self.segments[0].forward(MOVE_DISTANCE)
 
     def up(self):
-        self.segments[0].setheading(90)
+        self.head.setheading(90)
     def down(self):
-        self.segments[0].setheading(-90)
+        self.head.setheading(-90)
     def left(self):
-        self.segments[0].setheading(180)
+        self.head.setheading(180)
     def right(self):
-        self.segments[0].setheading(0)
+        self.head.setheading(0)
